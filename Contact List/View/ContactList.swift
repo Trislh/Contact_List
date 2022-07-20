6d11b674506729a9210c525d470370b662cecb98
Tri <s3462315@rmit.edu.vn>
2022-07-20 09:23:32 +0700

    Change file name

---
+++ Contact List/View/ContactList.swift
@@ -0,0 +1,20 @@
+//
+//  Contact_List.swift
+//  Contact List
+//
+//  Created by Tri, Luu Huynh on 20/07/2022.
+//
+
+import SwiftUI
+
+struct Contact_List: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Contact_List_Previews: PreviewProvider {
+    static var previews: some View {
+        Contact_List()
+    }
+}
