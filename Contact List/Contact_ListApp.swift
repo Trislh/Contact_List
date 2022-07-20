790a504d1716387c7be550511157472d174751bd
Tri <s3462315@rmit.edu.vn>
2022-07-20 08:54:54 +0700

    Initial Commit

---
+++ Contact List/Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_ListApp.swift
+//  Contact List
+//
+//  Created by Tri, Luu Huynh on 20/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
